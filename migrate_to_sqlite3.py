bc908f8c4ffe2bcfcdaa0da755ab56762d59a3dd
UTAGAWA Kiki <utagawakiki@gmail.com>
2019-09-27 23:48:53 +0900

    型アノテーション消す / スクリプト1つで完結するようにする

--- migrate_to_sqlite3.py
+++ migrate_to_sqlite3.py
@@ -1,10 +1,8 @@
-from typing import Any, Dict, List, Sequence, TypeVar, Union
 import sqlite3
 import nosponse
 DB_PATH = 'responses.sqlite3'
 
-T = TypeVar
-def response_to_list(response: Union[T, Sequence[T]]) -> List[T]:
+def response_to_list(response):
     """
     'A': B があるとき
     B=[C, D, E, ...] -> [C, D, E, ...]
@@ -15,18 +13,22 @@ def response_to_list(response: Union[T, Sequence[T]]) -> List[T]:
     else:
         return [response]
 
-def load_responses() -> Dict[str, Any]:
+def load_responses():
     return nosponse.j_file2dic("responses.json")
 
 def main():
-    conn = sqlite3.connect(DB_PATH)
     json_data = load_responses()
+    with sqlite3.connect(DB_PATH) as conn:
+        c = conn.cursor()
+        c.execute('CREATE TABLE response (msg, response)')
     for msg, res in json_data.items():
-        with conn as c:
+        with sqlite3.connect(DB_PATH) as conn:
+            c = conn.cursor()
             c.execute('insert into message values (?)', [msg])
         res = response_to_list(res)
         responses = [(msg, r) for r in res]
-        with conn as c:
+        with sqlite3.connect(DB_PATH) as conn:
+            c = conn.cursor()
             c.executemany('''
                 insert into response
                 values (?, ?)
